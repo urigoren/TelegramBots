4f0b85a7cc049f120a7644bf6f3499f7d41d2e3f
Uri Goren <uri@bla.sh>
2016-07-19 22:16:52 +0300

    Adding custom keyboards

--- picnic.py
+++ picnic.py
@@ -1,6 +1,7 @@
 import sys
 import telepot
 from telepot.delegate import per_chat_id, create_open
+from telepot.namedtuple import ReplyKeyboardMarkup, KeyboardButton
 
 with open('picnic.token','r') as f:
     TOKEN = f.readlines()[0].strip()
@@ -10,7 +11,7 @@ class PicnicBot(telepot.helper.ChatHandler):
     def __init__(self, seed_tuple, timeout):
         super(PicnicBot, self).__init__(seed_tuple, timeout)
         self.group=0
-        self.db={}
+        self.groceries={}
         self.sender2name={}
 
     def on_chat_message(self, msg):
@@ -38,29 +39,37 @@ class PicnicBot(telepot.helper.ChatHandler):
             return
         if response:
             self.sender.sendMessage(response)
+    def choose_items(self,txt,refer_to_command,counts=None):
+        if counts:
+            item_keyboard=[[KeyboardButton(text='/{r} {i} {c}'.format(r=refer_to_command,i=item,c=c)) for item in self.groceries.keys()] for c in counts]
+        else:
+            item_keyboard=[[KeyboardButton(text='/{r} {i}'.format(r=refer_to_command,i=item)) for item in self.groceries.keys()]]
+        self.sender.sendMessage(txt, reply_markup=ReplyKeyboardMarkup( keyboard=item_keyboard,one_time_keyboard=True  ))
 
     def add(self,sender,content):
         if content=='':
-            return 'Add what ?'
+            return 'Usage example:\n /add salad \n /add beer 6'
         try:
             item,count = content.split(' ',1)
             count = int(count)
         except:
             item = content
             count = 1
-        self.db[item] = {'total': count}
+        self.groceries[item] = {'total': count}
 
     def remove(self,sender,content):
         if content=='':
-            return 'Remove what ?'
+            self.choose_items('Remove what item ?','remove')
+            return None
         try:
-            del self.db[content]
+            del self.groceries[content]
         except:
             return
 
     def bring(self,sender,content):
         if content=='':
-            return 'Bring what ?'
+            self.choose_items('Bring what ?','bring',[1,2])
+            return None
         try:
             item,count = content.split(' ',1)
             count = int(count)
@@ -68,34 +77,56 @@ class PicnicBot(telepot.helper.ChatHandler):
             item = content
             count = 1
         try:
-            if sender in self.db[item].keys():
-                self.db[item][sender]+=count
+            if sender in self.groceries[item].keys():
+                self.groceries[item][sender]+=count
             else:
-                self.db[item][sender]=count
+                self.groceries[item][sender]=count
+            return self.sender2name[sender]+' is bringing {c} {i}'.format(c=count,i=item)
         except:
             return
 
-    def unbring(self,sender,content):
-        if content=='':
-            return 'Unbring what ?'
-        try:
-            item,count = content.split(' ',1)
-            count = int(count)
-        except:
-            item = content
-            count = 1
-        self.bring(sender,'{i} {c}'.format(i=item,c=-count))
+    def regret(self,sender,content):
+        ret=[]
+        for item in self.groceries.keys():
+            if sender in self.groceries[item].keys():
+                ret.append({'user':self.sender2name[sender],'item':item,'count':self.groceries[item][sender]})
+                del self.groceries[item][sender]
+        return '\n'.join(['{user} no longer brings {count} {item}'.format(**r) for r in ret])
 
-    def status(self,sender,content):
-        if content=='all':
-            return self.db
-        #remaining
-        ret={}
-        for item, counts in self.db.items():
+    def remaining(self,sender,content):
+        ret=[]
+        for item, counts in self.groceries.items():
             total = counts['total']
             remaining = 2*total - sum(counts.values())
-            ret[item]=remaining
-        return ret
+            ret.append('{i}: {r}'.format(i=item,r=remaining))
+        if len(ret)==0:
+            return 'Woohoo! No remaining items'
+        return 'Remaining items:'+'\n'.join(ret)
+
+    def my(self,sender,content):
+        ret=[]
+        for item, counts in self.groceries.items():
+            if not sender in counts.keys():
+                continue
+            ret.append('{i}: {c}'.format(i=item,c=counts[sender]))
+        if len(ret)==0:
+            return self.sender2name[sender]+", your picnic list is empty,\n Use /bring to take part in the picnic"
+        return self.sender2name[sender]+' brings:'+'\n'.join(ret)
+
+    def list(self,sender,content):
+        ret=[]
+        for item, counts in self.groceries.items():
+            total = counts['total']
+            ret.append('{i}:{t}'.format(i=item,t=total))
+        if len(ret)==0:
+            return "Picnic list is empty,\n Use /add to populate it"
+        return '\n'.join(ret)
+
+    def help(self,sender,content):
+        return '''Use /add and /remove to modify the picnic list
+        /bring and /regret to share what you bring
+        /list, /remaining and /my to view the current list status
+        '''
 
 
 bot = telepot.DelegatorBot(TOKEN, [
